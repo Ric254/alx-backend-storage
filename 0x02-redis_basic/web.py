eda9c9d8d9177d611eda684e5a32c606ba60f9ea
Ric254 <eerickmwangi@gmail.com>
2023-07-19 02:31:54 -0700

    correction

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,37 +1,29 @@
 #!/usr/bin/env python3
-"""web module
-"""
+"""Module tracks how many times a particular URL was accessed"""
 import redis
 import requests
 from typing import Callable
 from functools import wraps
 
-r = redis.Redis()
 
+def count(method: Callable):
+    '''track how many times a particular URL was accessed'''
+    r = redis.Redis()
 
-def count_requests(method: Callable) -> Callable:
-    """
-    count requests decorate
-    """
     @wraps(method)
     def wrapper(url):
-        """
-        Wrapper for decorator functionality
-        """
+        '''wrap the decorated function and return the wrapper'''
         r.incr(f"count:{url}")
-        cache_html = r.get(f"cached:{url}")
-        if cache_html:
-            return cache_html.decode('utf-8')
+        exp_count = r.get(f"cached:{url}")
+        if exp_count:
+            return exp_count.decode('utf-8')
         html = method(url)
         r.setex(f"cached:{url}", 10, html)
         return html
     return wrapper
 
 
-@count_requests
+@count
 def get_page(url: str) -> str:
-    """
-    get page method
-    """
-    request = requests.get(url)
-    return request.text
+    '''uses the requests module to obtain the HTML content'''
+    return requests.get(url).text
